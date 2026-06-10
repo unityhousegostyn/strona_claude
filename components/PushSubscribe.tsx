c4b33425cbcc4c4a1a606addd26878edebc0db15
Andrzej Mikulski <unity.housegostyn@gmail.com>
2026-06-10 09:59:44 +0200

    2FA, Excel export, search, PWA push, i18n, logo icons

--- components/PushSubscribe.tsx
+++ components/PushSubscribe.tsx
@@ -127,9 +127,13 @@ export default function PushSubscribe() {
   )
 }
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): ArrayBuffer {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = window.atob(base64)
-  return new Uint8Array([...rawData].map(c => c.charCodeAt(0)))
+  const output = new Uint8Array(rawData.length)
+  for (let i = 0; i < rawData.length; i++) {
+    output[i] = rawData.charCodeAt(i)
+  }
+  return output.buffer as ArrayBuffer
 }
